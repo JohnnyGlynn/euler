9d3f10aa10b46c6dc2541b9223939a003410f22f
Johnny <glynn_j@icloud.com>
2023-03-05 02:39:06 +0000

    While for finding multiples

---
+++ problems/problem1.go
@@ -0,0 +1,19 @@
+// Multiples of 3 or 5
+
+package problems
+
+import "fmt"
+
+func isitmultiple() {
+	fmt.Print("Finding multiples")
+
+	int sum
+
+	while(i != 1000){
+		
+		if(i%)
+
+		i++
+	}
+
+}
