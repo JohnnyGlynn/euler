f6bd32d5d7048e2371d6628435c385f032ee2337
Johnny <glynn_j@icloud.com>
2023-03-05 05:35:15 +0000

    Added probelm import example comment
    - Added example for referencing one problem from the main function
    - TODO: CLI

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"fmt"
 
 	archive "github.com/JohnnyGlynn/euler/archive"
+	//Uncomment to use specific problem solutions
+	//#####################################
+	// ac1_probelms "github.com/JohnnyGlynn/euler/archive/archive1/problems"
+	//#####################################
 )
 
 func main() {
@@ -14,6 +18,13 @@ func main() {
 
 	//Run all completed or in-progress archives
 	archive.Archive()
+
+	//Uncomment to use specific problem solutions
+	//TODO: User input problem slection, CLI?
+	//##########################
+	//problem specific function
+	// ac1_probelms.IsItMultiple()
+	//##########################
 
 	//Do some test stuff
 	//Possible problem selector?
